308a46e1c10773ef254354ba87bb91c604515a81
play4fun <play4fun@foxmail.com>
2017-12-23 15:11:34 +0800

    相位导入

--- py/config.py
+++ py/config.py
@@ -31,21 +31,58 @@ WN西北 = 7
 # print(NS北南,SN南北,SW南西,发动机)#可以用中文
 
 # 相位定义
-# 单向直行
-phaseA = [NS北南, SN南北]  # ||⬆️⬇️
-phaseA = [EW东西, WE西东]  # == ⏩️️⬅️
+# 双向直行
+phase_双向直行_南北 = [NS北南, SN南北]  # ||⬆️⬇️
+phase_双向直行_东西 = [EW东西, WE西东]  # == ⏩️️⬅️
 
 # 单向直行+左转
-phaseB = [SW南西, SN南北]  # 7|
-phaseB = [NS北南, NE北东]  # |L
+phase_单向直行_左转_南 = [SW南西, SN南北]  # 7|
+phase_单向直行_左转_北 = [NS北南, NE北东]  # |L
+phase_单向直行_左转_西 = [WN西北, WE西东]  # ➡️➡️⬆️⏩
 
 # 双向左转
-phaseC = [SW南西, NE北东]  # 7L
-phaseC = [WN西北, ES东南]  # ┘┌
+phase_双向左转_南北 = [SW南西, NE北东]  # 7L
+phase_双向左转_东西 = [WN西北, ES东南]  # ┘┌
 
 # 双向直行+左转
-phaseD = [EW东西, ES东南, WN西北, NE北东]
-phaseE = [NS北南, NE北东, SW南西, SN南北]
+phase_双向直行_左转_东西 = [EW东西, ES东南, WN西北, WE西东]
+phase_双向直行_左转_南北 = [NS北南, NE北东, SW南西, SN南北]
 
 # 十字路口信号相位
-cross1 = {'phase1': {}}
+cross1 = [{'phase': phase_双向直行_南北, 'time': 111, 'min': 35},
+          {'phase': phase_单向直行_左转_南, 'time': 9, 'min': 8},
+          {'phase': phase_双向左转_南北, 'time': 43, 'min': 10},
+          {'phase': phase_双向直行_左转_东西, 'time': 37, 'min': 35}
+          ]
+cross2 = [{'phase': phase_单向直行_左转_南, 'time': 28, 'min': 8},
+          {'phase': phase_双向直行_左转_南北, 'time': 121, 'min': 35},
+          {'phase': phase_单向直行_左转_北, 'time': 13, 'min': 8},
+          {'phase': phase_双向直行_左转_东西, 'time': 38, 'min': 35}
+          ]
+cross3 = [{'phase': phase_双向左转_南北, 'time': 28, 'min': 10},
+          {'phase': phase_双向直行_南北, 'time': 134, 'min': 35},
+          {'phase': phase_双向直行_左转_东西, 'time': 38, 'min': 35}
+          ]
+
+cross4 = [{'phase': phase_双向左转_南北, 'time': 34, 'min': 10},
+          {'phase': phase_双向直行_南北, 'time': 75, 'min': 35},
+          {'phase': phase_双向左转_东西, 'time': 31, 'min': 10},
+          {'phase': phase_单向直行_左转_西, 'time': 19, 'min': 8},
+          {'phase': phase_双向直行_东西, 'time': 41, 'min': 35}
+          ]
+
+cross5 = [{'phase': phase_双向左转_南北, 'time': 48, 'min': 10},
+          {'phase': phase_双向直行_南北, 'time': 89, 'min': 35},
+          {'phase': phase_双向直行_左转_东西, 'time': 63, 'min': 35}
+          ]
+
+cross6 = [{'phase': phase_双向左转_南北, 'time': 35, 'min': 10},
+          {'phase': phase_双向直行_南北, 'time': 67, 'min': 35},
+          {'phase': phase_双向左转_东西, 'time': 52, 'min': 10},
+          {'phase': phase_双向直行_东西, 'time': 46, 'min': 35}
+          ]
+
+cross7 = [{'phase': phase_双向左转_南北, 'time': 30, 'min': 10},
+          {'phase': phase_双向直行_南北, 'time': 96, 'min': 35},
+          {'phase': phase_双向直行_左转_东西, 'time': 74, 'min': 35}
+          ]
